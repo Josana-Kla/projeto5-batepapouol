639afdd7375ce913a163abc262b9baa6413229b2
Josana-Kla <josana.0205@gmail.com>
2022-06-26 16:49:20 -0300

    feat: getting all participants and creating post function

--- projeto5-batepapouol/scripts/main.js
+++ projeto5-batepapouol/scripts/main.js
@@ -1,10 +1,24 @@
-//prompt("Seu lindo nome:");
+let userName = prompt("Seu lindo nome:");
+let writeAMessage = document.querySelector('.message').value;
+
+
+function returningAllFunctions() {
+    gettingAllMessages();
+    returningAllParticipants();
+    postANewMessage();
+}
+
+returningAllFunctions();
+
+//--------------------------------------------------------------------------------------------//
+
+//Conecting with axios - GET
+function gettingAllMessages() {
+    let promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(catchDates);
+}
 
 let answers = [];
-
-//Conecting with axios
-let promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-promise.then(catchDates);
 
 function catchDates(answerDates) {
     console.log(answerDates.data);
@@ -29,7 +43,7 @@ function publicMessage() {
                 <div class="chat-line white-chat ">
                     <span>
                         <i>(${answers[i].time})</i>
-                        <strong>${answers[i].from}</strong> para <strong>${answers[i].to}</strong>:
+                        <strong>${answers[i].from}</strong> para <strong>${answers[i].to}</strong>: ${answers[i].text}
                     </span>
                 </div>
             `
@@ -44,7 +58,7 @@ function privateMessage() {
                 <div class="chat-line red-chat ">
                     <span>
                         <i>(${answers[i].time})</i> 
-                        <strong>${answers[i].from}</strong> reservadamente para <strong>${answers[i].to}</strong>: 
+                        <strong>${answers[i].from}</strong> reservadamente para <strong>${answers[i].to}</strong>: ${answers[i].text}
                     </span>
                 </div>
             `
@@ -52,21 +66,93 @@ function privateMessage() {
     }
 }
 
+//--------------------------------------------------------------------------------------------//
+
+// GET names of who entered in the room
+
+function returningAllParticipants() {
+    let promiseGetParticipantNames = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    promiseGetParticipantNames.then(catchNameParticipant);
+}
+
+let namesWhoEnter = [];
+
+function catchNameParticipant(nameParticipant) {
+    console.log(nameParticipant.data);
+    namesWhoEnter = nameParticipant.data;
+    renderNames();
+}
+
+function renderNames() {
+    chat.innerHTML = '';
+    for(let i = 0; i < namesWhoEnter.length; i++) {
+        chat.innerHTML += `
+            <div class="chat-line grey-chat">
+                <span>
+                    <i>(HORA)</i>
+                    <strong>${namesWhoEnter[i].name}</strong> entra na sala...
+                </span>
+            </div>
+        `
+    }
+}
+
+//--------------------------------------------------------------------------------------------//
+
+//POST with axios 
+function postANewMessage() {
+    let promisePost = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promisePost.then(renderPosts);
+}
+
+function renderPosts() {
+    iEnteredInTheRoom();
+    postAMessage();
+}
+
+function iEnteredInTheRoom() {
+    chat.innerHTML = '';
+    chat.innerHTML += `
+        <div class="chat-line grey-chat">
+            <span>
+                <i>(HORA)</i>
+                <strong>${userName}</strong> entra na sala...
+            </span>
+        </div>
+    `
+}
 
 
-// let answersParticipants;   fazer o POST disso
+function postAMessage() {
+  const newMessage = {
+    from: `${userName}`,
+    to: "Todos",
+    text: `${writeAMessage}`,
+    type: "message" // ou "private_message" para o bônus   
+  }
+  console.log(newMessage);
+}
+
+// let answersParticipants;   //fazer o POST disso
 
 // function statusEnterInTheRoom() {
 //     const newChat = document.querySelector('main');
 
-//     if(answers[i].type === "status") {
-//         newChat.innerHTML += `
-//             <div class="chat-line grey-chat">
-//                 <span>
-//                     <i>(${answers[i].time})</i>
-//                     <strong>${answersParticipants[i].name}</strong> entra na sala...
-//                 </span>
-//             </div>
-//         `
+//     for(let i = 0; i < nameWhoEnter.length; i++) {
+//         if(answers[i].type === "status") {
+//             newChat.innerHTML += `
+//                 <div class="chat-line grey-chat">
+//                     <span>
+//                             <i>(${answers[i].time})</i>
+//                         <strong>${answersParticipants[i].name}</strong> entra na sala...
+//                     </span>
+//                 </div>
+//             `
+//         }
+
 //     }
 // }
+
+
+
+//--------------------------------------------------------------------------------------------//
